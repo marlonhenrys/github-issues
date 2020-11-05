5c94a49328e90ba60d8384b8a8939503ca76fde9
Lucas <lucas.zero55@hotmail.com>
2020-11-04 21:25:10 -0300

    First try to make the SO issue mining

--- src/app/minePosts.js
+++ src/app/minePosts.js
@@ -1,9 +1,31 @@
 // usar isso no helper fetch (criar nova função)
-const { stackOverflow } = require('../config/request')
+const axios = require('axios');
+const fs = require('fs');
+const file = require('../helpers/file')
+
+const api = axios.create({
+    baseURL: 'https://api.stackexchange.com'
+})
 
 module.exports = async params => {
-    console.log(params, stackOverflow)
+    const issues = [];
+    const soIssues = [];
 
-    // criar nova função para posts nos helpers 'format' e 'file' 
-    // (usar o params.filename - o valor dele é 'posts')
+    var fileContents = fs.readFileSync('data/issues.csv');
+    var lines = fileContents.toString().split('\n');
+
+    for (var i = 0; i < lines.length; i++) {
+        issues.push(lines[i].toString().split(','));
+    }
+
+    for(const issue of issues){
+        const res = await api.get(`/2.2/search/advanced?order=desc&sort=activity&q=${issue[0]} ${issue[1]}&site=stackoverflow`)
+        const formattedNodes = {
+            repo: issue[0],
+            issueNumber: issue[1],
+            foundOnSO: res.data && res.data.length
+        }
+        soIssues.push(formattedNodes)
+    }
+    await file.save(soIssues, params.filename)
 }
